6ac86f4ad00ab9288b345ec8912e6ba234834cab
aislaghatub <osullia5@tcd.ie>
2019-08-05 17:42:47 -0400

    Update Step1_import_bdf.m
    added text

--- Step1_import_bdf.m
+++ Step1_import_bdf.m
@@ -11,7 +11,7 @@ subject_name='EL';
 bdf_file_path=[study_path 'bdfFiles\'];
 EEG=pop_biosig([bdf_file_path subject_name '.bdf']); % if not installed, dialog will open-> confirm installation
 % there might few warnings e.g. but ignore them
-if ~isa(EEG.data,'double'); error('Data loaded as SINGLE. Change EEGLAB settings'); end % for slow people....
+if ~isa(EEG.data,'double'); error('Data loaded as SINGLE. Change EEGLAB settings and run again!'); end % for slow people....
 
 mat_file_path=[study_path 'eegMatFiles\'];
 if ~exist(mat_file_path, 'dir'); mkdir(mat_file_path); end % create folder if does not exist
